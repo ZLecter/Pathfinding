b907ec6f4b2624bf56a163581ca53a3fda5c4d73
ZLecter <gzlecter@gmail.com>
2018-08-22 13:10:27 -0500

    Implemented grid and nodes

--- board.js
+++ board.js
@@ -1,12 +1,65 @@
 class Board {
+	/**
+	 *Creates an instance of Board.
+	 * @param {number} w Width of the board
+	 * @param {numer} h Height of the board
+	 */
 	constructor(w, h){
 		this.w = w; // cols
 		this.h = h; // width
 
-		this.board = Create2DArray(w, h);
+		this.node = Create2DArray(w, h);
+	}
+
+	
+	/**
+	 * @param {Node} node Node to calculate neighbors
+	 * @returns {*} Valid neighbors from node
+	 */
+	CalculateNeighbors(nodeBase){
+		if(!nodeBase.isWalkable){
+			return [];
+		}
+
+		// Up, right, down and left
+		// Can be multidirectional
+		const dirs = [[1,0], [0,1], [-1,0], [0,-1]];
+		let neighbors = new Array();
+		dirs.forEach(dir => {
+			let x = nodeBase.x + dir[0];
+			let y = nodeBase.y + dir[1];
+			if( (x >= 0 && x < this.w) && (y >= 0 && y < this.h) ){
+				let n = this.node[x][y];
+				if(n.isWalkable){
+					neighbors.push(n);
+				}
+			}
+		});
+		return neighbors;
 	}
 }
 
+class Node {
+	/**
+	 *Creates an instance of Node.
+	 * @param {number} x X Position
+	 * @param {number} y Y Position
+	 * @param {boolean} isWalkable
+	 */
+	constructor(x, y, isWalkable) {
+		this.x = x;
+		this.y = y;
+		this.isWalkable = isWalkable;
+
+		this.neighbors = new Array();
+	}
+}
+
+/**
+ * @param {number} nCols Number of Columns
+ * @param {number} nRows Number of Rows
+ * @returns {*} 2D array of [nRows][nCols]
+ */
 function Create2DArray(nCols, nRows){
 	let array = new Array(nRows);
 	for(let i = 0; i < nCols; i++){
--- index.js
+++ index.js
@@ -13,7 +13,15 @@ function setup(){
 
 	for (let i = 0; i < board.w; i++) {
 		for (let j = 0; j < board.h; j++) {
-			board.board[i][j] = true;
+			board.node[i][j] = new Node(i, j, true);
+		}
+	}
+	board.node[1][1].isWalkable = false;
+
+	for (let i = 0; i < board.w; i++) {
+		for (let j = 0; j < board.h; j++) {
+			let n = board.node[i][j];
+			n.neighbors = board.CalculateNeighbors(n);
 		}
 	}
 }
@@ -22,10 +30,7 @@ function draw(){
 
 	for (let i = 0; i < board.w; i++) {
 		for (let j = 0; j < board.h; j++) {
-			// let r = map(i, 0, board.w-1, 0, 255);
-			// let g = map(j, 0, board.h-1, 0, 255);
-			// fill(r, g, 0, 255);
-			let isWalkable = board.board[i][j];
+			let isWalkable = board.node[i][j].isWalkable;
 			if(isWalkable) fill(255);
 			else fill(0,0,0);
 
@@ -33,23 +38,51 @@ function draw(){
 		}
 	}
 
+	// Draw player
 	fill(25, 125, 125);
 	ellipse(player.x * scaleX + scaleX/2, player.y * scaleY + scaleY/2, scaleX * 0.9);
-	
+	// Draw End Point
 	fill(225, 0, 125);
 	ellipse(end.x * scaleX + scaleX/2, end.y * scaleY + scaleY/2, scaleX * 0.9);
-	
+
+	// Debug for neighbors
+	let xpos = parseInt(mouseX/scaleX);
+	let ypos = parseInt(mouseY/scaleY);
+	if((xpos >= 0 && xpos < board.w) && (ypos >= 0 && ypos < board.h)){
+		fill(0,255,0, 128);
+		rect(xpos * scaleX, ypos * scaleY, scaleX, scaleY);
+		board.node[xpos][ypos].neighbors.forEach(n => {
+			fill(0,0, 255, 128);
+			rect(n.x * scaleX, n.y * scaleY, scaleX, scaleY);
+		});
+	}
 }
 
 function mousePressed(){
 
 	let xpos = parseInt(mouseX/scaleX);
 	let ypos = parseInt(mouseY/scaleY);
-	if(xpos != player.x || ypos != player.y){
-		if(xpos != end.x || ypos != end.y){
-			let isWalkable = board.board[xpos][ypos];
-			board.board[xpos][ypos] = !isWalkable;
+
+	if((xpos >= 0 && xpos < board.w) && (ypos >= 0 && ypos < board.h)){
+		if(xpos != player.x || ypos != player.y){
+			if(xpos != end.x || ypos != end.y){
+				let n = board.node[xpos][ypos];
+				n.isWalkable = !n.isWalkable;
+				// Recalculate neighbors if node changed
+				if(n.isWalkable){
+					n.neighbors = board.CalculateNeighbors(n);
+					n.neighbors.forEach(nb => {
+						nb.neighbors = board.CalculateNeighbors(nb);
+					});
+				}else{
+					n.neighbors.forEach(nb => {
+						nb.neighbors = board.CalculateNeighbors(nb);
+					});
+					n.neighbors = board.CalculateNeighbors(n);
+				}
+			}
 		}
 	}
+	
 	
 }
